b5cdff871d91620d26125825772b14a3c71b73cc
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-24 22:57:48 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        String name = "Adithya";
+        System.out.println(name);
+        a=b;
+        System.out.println(b);
+
+    }
+    
+}

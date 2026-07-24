211c1fd3e6a97b456a380c9b60dcfce36a1aa476
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-24 23:22:07 +0530

    Add files via upload

---
+++ Sumoftwonumbers.java
@@ -0,0 +1,9 @@
+public class Sumoftwonumbers {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        int sum = a+b;
+        System.out.println("The sum of two numbers is" +  sum);   
+     }
+    
+}

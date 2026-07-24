9c8dec5622c3b32624b0d0c627291729e3d77ac4
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-24 22:50:28 +0530

    Add files via upload

---
+++ javaprogramming.java
@@ -0,0 +1,9 @@
+public class javaprogramming {
+    public static void main(String args[]) {
+        System.out.print("Hello Adithya!\n");
+        System.out.print("Hello Adithya!\n");
+        System.out.print("Hello Adithya!\n");
+
+
+    }
+}

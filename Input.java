335ed5afe29084cb8740aaaec564077e39c6ca53
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-25 23:03:03 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+       // String input = sc.next();
+        //System.out.println(input);
+
+       // String name = sc.nextLine();
+        //System.out.println(name);
+
+       // int number = sc.nextInt();
+       // System.out.println(number);
+
+       Float price = sc.nextFloat();
+       System.out.println(price);
+    }
+}

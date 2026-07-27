979ff44b754d4917687eea0adec1db8b5810f61b
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-27 22:37:13 +0530

    Add files via upload

---
+++ Arithmeticoperator.java
@@ -0,0 +1,11 @@
+public class Arithmeticoperator {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum is " + (a+b));
+        System.out.println("subtract is " + (a-b));
+        System.out.println("Mul is " + (a*b));
+        System.out.println("div is " + (a/b));
+        System.out.println("modulo is " + (a%b));
+    }
+}

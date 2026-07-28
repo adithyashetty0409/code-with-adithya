3e8215bf6626e3888b410208cc80824b21164dd0
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-28 22:31:20 +0530

    Add files via upload

---
+++ Assignmentoperator.java
@@ -0,0 +1,24 @@
+public class Assignmentoperator {
+    public static void main(String args[]) {
+        int a = 10;
+       // a = a+10;
+       a +=10;
+        System.out.println(a);
+         int b = 10;
+       // b = b-10;
+       b -=10;
+        System.out.println(b);
+         int c = 10;
+       // c = c*10;
+       c *=10;
+        System.out.println(c);
+         int d = 10;
+       // d = d/10;
+       d /=10;
+        System.out.println(d);
+         int x = 10;
+       // x = x+10;
+       x %=10;
+        System.out.println(x);
+    }
+}

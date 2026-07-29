142e5095406e187313eba74c9f53a28ba9c981e0
Adithya Shetty <adithyashetty9579@gmail.com>
2026-07-29 23:10:22 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,16 @@
+public class condition {
+    public static void main(String[] args) {
+        
+        int age = 21;
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } 
+        else if (age > 13 && age < 18) {
+            System.out.println("Teenager");
+        } 
+        else {
+            System.out.println("Not an adult");
+        }
+    }
+}

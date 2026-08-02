84a1b5abf2f8020c24ebe3da73176bd88c647b40
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-02 13:40:52 +0530

    Add files via upload

---
+++ passorfail.java
@@ -0,0 +1,10 @@
+public class passorfail {
+    
+    public static void main(String args[]) {
+        int marks = 36;
+        String type = (marks < 35 )? "fail":"pass";
+        System.out.println(type);
+    }
+}
+
+

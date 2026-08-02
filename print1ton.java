cdb1b9612ffc084d8ce5c97d920f3903279a689b
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-02 23:09:58 +0530

    Add files via upload

---
+++ print1ton.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class print1ton {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the range");
+        int range = sc.nextInt();
+        int counter = 1
+;
+        while(counter <= range) {
+            System.out.print(counter + " ");
+            counter++;
+        }
+    }
+}

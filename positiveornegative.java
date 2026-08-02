a535c4982d912a3767415f5c18e02ed20a1311af
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-02 17:42:18 +0530

    Add files via upload

---
+++ positiveornegative.java
@@ -0,0 +1,14 @@
+public class positiveornegative {
+    public static void main(String[] args) {
+        int a =10;
+        if(a < 0) {
+            System.out.println("The number is negative");
+        
+
+        }    
+        else {
+            System.out.println("The number is positive");
+        }
+        }
+    }
+
---
+++ switchstatement.java
@@ -0,0 +1,18 @@
+public class switchstatement {
+    public static void main(String[] args) {
+        int i = 31;
+        switch(i)
+        {
+            case 1: 
+            System.out.println("Samosa");
+               break;
+            case 2:
+                System.out.println("mango shake");   
+                break;
+            case 3:
+                System.out.println("burger");    
+            default:
+                System.out.println("we woke up");    
+        }
+    }
+}

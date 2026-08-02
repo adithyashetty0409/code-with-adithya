af12690a225153865b31535a77cdbc0ff22b836e
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-02 13:16:28 +0530

    Add files via upload

---
+++ ternaryoperator.java
@@ -0,0 +1,7 @@
+public class ternaryoperator {
+    public static void main(String args[]) {
+        int a =11;
+        String type = ((a%2)==0)? "true":"false";
+        System.out.println(type);
+    }
+}

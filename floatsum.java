972fbcb8fcff8658dc64daacbcafbd01e38d5c43
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-18 23:19:29 +0530

    Add files via upload

---
+++ floatsum.java
@@ -0,0 +1,13 @@
+public class floatsum {
+    public static int sum(int a , int b) {
+        return a+b;
+    }
+    public static float sum(float x , float y) {
+        return x+y;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum(12,34));
+        System.out.println(sum(23.45f,345.56f));
+    }
+    
+}

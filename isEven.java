35fb3c057935624e1ab416738b1632e17180b1e2
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-26 12:46:12 +0530

    Add files via upload

---
+++ isEven.java
@@ -0,0 +1,13 @@
+public class isEven {
+    public static boolean isEven(int n) {
+        return n%2 ==0;
+
+    }
+    public static void main(String[] args) {
+        
+    
+    System.out.println(isEven(10));
+System.out.println(isEven(7));
+    
+}
+}

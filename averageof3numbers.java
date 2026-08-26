ede2da7e6c94dd433853fc8d3d3114a0ae60eda7
Adithya Shetty <adithyashetty9579@gmail.com>
2026-08-26 12:45:54 +0530

    Add files via upload

---
+++ averageof3numbers.java
@@ -0,0 +1,10 @@
+public class averageof3numbers {
+
+    public static double average(int a, int b, int c) {
+        return (a + b + c) / 3.0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(average(3, 4, 6));
+    }
+}

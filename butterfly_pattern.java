cf64f86d4520f47fcf134feb16593db7dee8ba35
Adithya Shetty <adithyashetty9579@gmail.com>
2026-09-02 23:44:33 +0530

    Add files via upload

---
+++ butterfly_pattern.java
@@ -0,0 +1,45 @@
+public class butterfly_pattern {
+
+    public static void butterfly(int n) {
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        butterfly(5);
+    }
+}
---
+++ triangle01.java
@@ -0,0 +1,26 @@
+public class triangle01 {
+
+    public static void triangle0_1(int n) {
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        triangle0_1(n);
+    }
+}
